f088f7c77eaba54c18e1e06f55b190c38436e2bc
Andrew Muto <amuto77@gmail.com>
2020-10-06 12:14:21 -0400

    MVP

--- api/server.js
+++ api/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const db = require("../data/dbConfig.js");
 
+const accountRouter = require('./accountRouter')
+
 const server = express();
 
 server.use(express.json());
